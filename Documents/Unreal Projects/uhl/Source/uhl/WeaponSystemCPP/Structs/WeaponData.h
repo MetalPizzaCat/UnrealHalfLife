f1bc82227f370ec18a0e53c9fa5b07739280e444
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2019-07-16 14:37:43 +1000

    Added WeaponData, BaseFireComponent

---
+++ Documents/Unreal Projects/uhl/Source/uhl/WeaponSystemCPP/Structs/WeaponData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedStruct.h"
+#include "WeaponData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UHL_API UWeaponData : public UUserDefinedStruct
+{
+	GENERATED_BODY()
+	
+};
---
+++ Documents/Unreal Projects/uhl/Source/uhl/WeaponSystemCPP/WeaponFireComponents/WeaponFireComponent.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UHL_API WeaponFireComponent
+{
+public:
+	WeaponFireComponent();
+	~WeaponFireComponent();
+};
